1d2b8ca9229d5fe994ae60df8f5a5d7de90a8ac4
Raghav Gupta <raghav.gupta8435@gmail.com>
2021-11-21 18:42:15 +0530

    implemented indexed db and gallery element

---
+++ gallery.js
@@ -0,0 +1,45 @@
+setTimeout(() => {
+
+    if(db) {
+        //video retireval
+        //image retireval
+    
+        let dbTransaction = db.transaction("video", "readonly");
+        let videoStore = dbTransaction.objectStore("video");
+        let videoRequest = videoStore.getAll();
+        videoRequest.onsuccess = (e) => {
+            let videoResult = videoRequest.result;
+            let galleryCont = document.querySelector(".gallery-cont");
+
+            videoResult.forEach(videoObj => {
+                let mediaElem = document.createElement("div");
+                mediaElem.setAttribute("class", "media-cont");
+                mediaElem.setAttribute("id", videoObj.id);
+
+                let url = URL.createObjectURL(videoObj.bolbData);
+
+                mediaElem.innerHTML = `
+                <div class="media">
+                    <video autoplay loop src="${url}"></video>
+                </div>
+                <div class="delete action-btn">DELETE</div>
+                <div class="download action-btn">DOWNLOAD</div>
+                `;
+
+                galleryCont.appendChild(mediaElem);
+
+            })
+    
+        }
+    }
+
+},100)
+
+
+
+
+
+
+
+
+
